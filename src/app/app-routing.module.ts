789e970bc66045871a33916a78b56b6afef90177
Budesh <bpatil775@gmail.com>
2023-01-23 15:49:19 +0530

    code commit for AOS issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-{
-  path:"about",
-  component:AboutComponent
-},{
-  path: "",
-  component:HomeComponent
-}
+
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+  , {
+    path: 'about',
+    component: AboutComponent
+  }
 ];
 
 @NgModule({
